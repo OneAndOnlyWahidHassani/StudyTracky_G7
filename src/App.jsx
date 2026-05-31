50b0c2d9fed76126fb19114bd6e6efcde56aa5b7
Wahid Hassani <Wahid_hassani@outlook.com>
2026-05-31 09:50:25 +0200

    lite mer byggande

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
-  
+function AppInner(theme, onToggleTheme) {
+
 }
 
-export default App
+export default function App() {
+  const { //lägg in typ tema och funktion för att byta tema} = useState()
+    }
+
+
+}
